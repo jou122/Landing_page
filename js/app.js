90f6f73160eb1f807b47ce8345ea20c5427e2780
jou122 <36369688+jou122@users.noreply.github.com>
2021-08-25 02:48:04 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,55 +1,53 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
+let navbar = document.getElementById('navbar__list');
+let sections =[...document.getElementsByTagName('section')];
+let sectionNumbers=sections.length;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+//function to create the navigation bar
+function createNavBar(){
+  for(const [i,section] of sections.entries()){
+    
+    let sectionListItem = document.createElement('li');
+    let sectionlink = document.createElement('a');
+    sectionlink.className = 'menu__link'
+    sectionlink.href="#"+section.getAttribute('id') ;
+    sectionlink.innerText="Section "+ (i+1);
+    sectionListItem.appendChild(sectionlink);
+    navbar.appendChild(sectionListItem);
+    
+  }
+
+}
+//function to change class of the section in view
+function activeClass(){
+  for(const section of sections){
+    let sectionCoordinate=section.getBoundingClientRect();
+    if (
+	sectionCoordinate.top >= -1) {
+        section.classList.add("your-active-class");
+      }
+    else{section.classList.remove("your-active-class");}
+  }
+}
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
+//function to hide Navbar
+function hideNavbar(){
+  document.getElementById("navbar").style.top = "-70px";
+}
 
 
-// Add class 'active' to section when near top of viewport
+//function to detect if user not scrolling
+function sleep(){
+   document.getElementById("navbar").style.top = "0px";
+var myVar;
+ myVar = setTimeout(hideNavbar, 4000);
+}
 
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+//main function
+document.addEventListener('scroll',activeClass);
+document.addEventListener('scroll',sleep);
+createNavBar();
